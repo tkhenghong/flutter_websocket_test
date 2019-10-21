388aa378ae284bdd6d291fa900eea5161e87768b
Teoh Kheng Hong <tkhenghong@gmail.com>
2019-10-21 08:22:19 +0800

    Websocket in progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:collection';
+import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
@@ -7,18 +8,23 @@ import 'package:web_socket_channel/io.dart';
 import 'package:flutter/material.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:http/http.dart' as http;
+//import 'package:socket_flutter_plugin/socket_flutter_plugin.dart';
+import 'package:alice/alice.dart';
+import "package:stomp/stomp.dart";
+import "package:stomp/vm.dart" show connect;
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     final title = 'WebSocket Demo';
     return MaterialApp(
       title: title,
       home: MyHomePage(
-        title: title,
-        channel: IOWebSocketChannel.connect('ws://192.168.88.165:8080/topic/public'),
+        title: title, // 'ws://192.168.0.139:8080/gs-guide-websocket'
+        channel: IOWebSocketChannel.connect("ws://192.168.0.139:8080/gs-guide-websocket"),
       ),
     );
   }
@@ -28,8 +34,7 @@ class MyHomePage extends StatefulWidget {
   final String title;
   final WebSocketChannel channel;
 
-  MyHomePage({Key key, @required this.title, @required this.channel})
-      : super(key: key);
+  MyHomePage({Key key, @required this.title, @required this.channel}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -37,9 +42,50 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   TextEditingController _controller = TextEditingController();
+  Alice alice;
+  StompClient client;
+//  SocketFlutterPlugin myIO;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    alice = Alice(showNotification: true);
+    super.initState();
+
+  }
+
+  void _runHttpInspector() {
+    alice.showInspector();
+  }
+
+  testWebsocketPlugin() async {
+    print("testWebsocketPlugin()");
+    StompClient client = await connect("192.168.0.139", port: 8080, host: "192.168.0.139", onConnect: (StompClient client, Map<String, String> headers) {
+      print("onConnect()");
+    }, onError: (StompClient client, String message, String detail, Map<String, String> headers) {
+      print("onError()");
+    }, onFault: (StompClient client, error, stackTrace) {
+      print("onFault()");
+      print("client: " + client.toString());
+      print("error: " + error.toString());
+      print("stackTrace: " + stackTrace.toString());
+    }, );
+    print("Connected.");
+    this.client = client;
+    client.subscribeString("/topic/greetings", "/gs-guide-websocket",
+            (Map<String, String> headers, String message) {
+
+          print("Recieve $message");
+        });
+
+    print("Sending testwebsocket string");
+    client.sendString("/group/0129137868", "Hi, Stomp");
+  }
 
   @override
   Widget build(BuildContext context) {
+//    testNewWebsocketPlugin();
+    testWebsocketPlugin();
     Stream<dynamic> stream = widget.channel.stream.asBroadcastStream();
     stream.listen((onData) {
       print("onData listener is working.");
@@ -51,52 +97,60 @@ class _MyHomePageState extends State<MyHomePage> {
       print("onDone listener is working.");
     }, cancelOnError: false);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Form(
-              child: TextFormField(
-                controller: _controller,
-                decoration: InputDecoration(labelText: 'Send a message'),
-              ),
-            ),
-            StreamBuilder(
-              stream: stream,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  print("if(snapshot.hasData)");
-                  print("snapshot.data: " + snapshot.data.toString());
-                } else {
-                  print("if(!snapshot.hasData)");
-                }
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 24.0),
-                  child: Text(snapshot.hasData ? '${snapshot.data}' : ''),
-                );
-              },
-            )
-          ],
+    return MaterialApp(
+      navigatorKey: alice.getNavigatorKey(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Form(
+                child: TextFormField(
+                  controller: _controller,
+                  decoration: InputDecoration(labelText: 'Send a message'),
+                ),
+              ),
+              StreamBuilder(
+                stream: stream,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    print("if(snapshot.hasData)");
+                    print("snapshot.data: " + snapshot.data.toString());
+                  } else {
+                    print("if(!snapshot.hasData)");
+                  }
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 24.0),
+                    child: Text(snapshot.hasData ? '${snapshot.data}' : ''),
+                  );
+                },
+              ),
+              RaisedButton(
+                child: Text("Run HTTP Insepctor"),
+                onPressed: _runHttpInspector,
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _sendMessage,
+          tooltip: 'Send message',
+          child: Icon(Icons.send),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _sendMessage,
-        tooltip: 'Send message',
-        child: Icon(Icons.send),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
   void _sendMessage() {
     print("Send Message");
     if (_controller.text.isNotEmpty) {
-      addConversationGroup(_controller.text);
+      sendMessageToREST(_controller.text);
       widget.channel.sink.add(_controller.text);
+      this.client.sendString("/app/hello", _controller.text);
     }
   }
 
@@ -106,8 +160,8 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
-  addConversationGroup(String message) async {
-    String wholeURL = "http://192.168.88.165:8080" + "/chat.sendMessage";
+  sendMessageToREST(String message) async {
+    String wholeURL = "http://192.168.0.139:8080" + "/testingWEbsocket";
     var httpResponse = await http.post(wholeURL, body: message, headers: createAcceptJSONHeader());
 
     print("HTTP RESPONSE");
@@ -128,7 +182,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   bool httpResponseIsCreated(Response httpResponse) {
-    if (httpResponse.statusCode == 201) {
+    if (httpResponse.statusCode == 200) {
       return true;
     } else {
       print("Request failed with status: ${httpResponse.statusCode}.");
